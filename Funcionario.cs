21d03d10d3a04c66ef800adc0cc9000491a92dfc
leonardo fernandes <leonardo fernandes@LAPTOP-VL5MJIVC>
2022-02-07 15:48:57 -0300

    finalizado

---
+++ Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_listas
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public double Sal { get; set; }
+
+        public Funcionario(int id, string nome, double sal)
+        {
+            Id = id;
+            Nome = nome;
+            Sal = sal;
+        }
+
+        public void NovoSal(double porc)
+        {
+            Sal += porc / 100 * Sal;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + Sal;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exercicio_listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos funcionarios serão resgistrados: ");
+            int qnt = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 0; i < qnt; i++)
+            {
+                Console.WriteLine("Funcionario #" + i);
+                Console.WriteLine("Nome: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("ID");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Salario");
+                double sal = double.Parse(Console.ReadLine());
+                list.Add(new Funcionario(id, nome, sal));
+
+            }
+
+            Console.WriteLine("Entre o id do funcionario");
+            int Idplus = int.Parse(Console.ReadLine());
+
+            Funcionario func = list.Find(x => x.Id == Idplus);
+            if (func != null)
+            {
+                Console.WriteLine("Entre a porcentagem do aumento do salario");
+                double porc = double.Parse(Console.ReadLine());
+                func.NovoSal(porc);
+            }
+            else
+            {
+                Console.WriteLine("Não existe esse id");
+            }
+
+            Console.WriteLine("Lista de funcionarios: ");
+            foreach (Funcionario obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+
+
+
+        }
+    }
+}
